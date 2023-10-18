17af4afe7019cbbc60e64cfb61fb4c5760237e2c
AlausaKabir <alausakabir0@gmail.com>
2023-10-18 14:32:53 +0100

    Chore: First Commit

---
+++ app/config/keys.js
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const Port = process.env.PORT
+
+const keys = {
+    environment: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 4000,
+    domain: process.env.DOMAIN || `http://localhost:${Port}`,
+    BCRYPT: process.env.BCRYPT || 10,
+    adminUrl: process.env.ADMIN_URL || 'admin',
+    jwt: {
+        refresh: process.env.JWT_REFRESH_TOKEN
+    },
+    database: {
+        mongoDb: {
+            development: {
+                connectionString: process.env.MONDO_URI_DEV
+            }
+        }
+    }
+
+}
---
+++ app/routes/index.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import morgan from 'morgan'
+
+
+const app = express()
+
