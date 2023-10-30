7e2dc04bfa1ce4567e19da1ea5c65c779109e6ed
AlausaKabir <alausakabir0@gmail.com>
2023-10-30 21:15:24 +0100

    Chore: Matriculation formulation

--- app/utils/constants/options.js
+++ app/utils/constants/options.js
@@ -10,14 +10,26 @@ const STAFF_TYPES = {
     NON_ACADEMIC: 'NonAcademicStaff'
 }
 
-const STUDENT_STATUS = { 
+const STUDENT_STATUS = {
     ACTIVE: 'active',
     GRADUATED: 'graduated',
     WITHDRAWN: 'withdrawn',
     EXPELLED: 'expelled'
 }
 
+const COURSE_CODE = {
+    "Accounting": "01",
+    "Business Administration": "02",
+    "Insurance": "03",
+    "Marketing": "04",
+    "Mtech": "05",
+    "BFN": "06",
+    "HRM": "07",
+    "Public Admin": "08",
+};
+
 export {
+    COURSE_CODE,
     STUDENT_TYPES,
     STAFF_TYPES,
     STUDENT_STATUS
--- app/utils/response.js
+++ app/utils/response.js
@@ -8,7 +8,7 @@ class ApiResponse {
      * @param {data} object - The success data
      * @returns {res} object - The response object
      */
-    
+
     static successResponse(res, statusCode, message, data, token = undefined) {
         return res.status(statusCode).json({
             error: false,
@@ -26,4 +26,12 @@ class ApiResponse {
      * @param {statusCode} number - The status code
      * @param {message} string - The error message, or a note explaining the error
      */
+
+    static errorResponse(res, statusCode, message) {
+        return res.status(statusCode).json({ error: true, status: statusCode, message })
+    }
 }
+
+const { successResponse, errorResponse } = ApiResponse
+
+export { successResponse, errorResponse }
