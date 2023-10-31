3fc9a5502d945cebdd37b9316d787b7311a36bfe
AlausaKabir <alausakabir0@gmail.com>
2023-10-31 14:10:43 +0100

    Chore: Matriculation No generation completed

--- app/utils/constants/students-matric.js
+++ app/utils/constants/students-matric.js
@@ -1,25 +1,34 @@
 import COURSE_CODE from './options.js'
 // Modify the generateMatriculationNumber function to accept department names
-function generateMatriculationNumber(department, registrationCount) {
-    // Get the current date to obtain the month part
-    const currentDate = new Date();
-    const monthPart = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+/**
+ * @description Matriculation No. Generation
+ */
 
-    // Ensure the course code is a two-digit number using the mapping
-    const coursePart = COURSE_CODE[department] || "00";
+class MatriculationNumber {
+    /**
+     * 
+     * @param {*} course - The Course of Students 
+     * @param {*} registrationCount - Serial generations
+     * @returns 
+     */
 
-    // Ensure the registration count is a two-digit number
-    const registrationPart = registrationCount.toString().padStart(2, '0');
+    static generateMatriculationNumber(course, registrationCount) {
+        // Get the current date to obtain the month part
+        const currentDate = new Date();
+        const monthPart = (currentDate.getMonth() + 1).toString().padStart(2, '0');
 
-    // Concatenate the parts to form the matriculation number
-    const matriculationNumber = `${monthPart}${coursePart}${registrationPart}`;
+        // Ensure the course code is a two-digit number using the mapping
+        const coursePart = COURSE_CODE[course] || "00";
 
-    return matriculationNumber;
+        // Ensure the registration count is a two-digit number
+        const registrationPart = registrationCount.toString().padStart(2, '0');
+
+        // Concatenate the parts to form the matriculation number
+        const matriculationNumber = `${monthPart}${coursePart}${registrationPart}`;
+
+        return matriculationNumber;
+    }
+
 }
 
-// Example usage with department name
-const department = "Insurance"; // For the Insurance department
-const registrationCount = 7; // For the 7th registration in this department
-
-const matriculationNumber = generateMatriculationNumber(department, registrationCount);
-console.log(matriculationNumber);
+export { MatriculationNumber }
